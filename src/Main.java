6485c3a67be32c5c4721d838dafb7447a13ce938
Mailon Barbetta <90325470+MailonB@users.noreply.github.com>
2024-08-19 01:04:29 +0000

    //tentando testar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
+import Servers.ServerBase;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Robson");
+
+    public static void main(String[] args) throws IOException {
+       
+        ServerBase serverBase = new ServerBase();
+        serverBase.initServer();
     }
 }
--- src/Services/ServiceBase.java
+++ src/Services/ServiceBase.java
@@ -4,4 +4,6 @@ public class ServiceBase {
 
     //Talvez da pra usar isso como uma base realmente e deixar como uma classe abstrata que tem as funçoes que os outros utilizam
     // Só pra ficar bonito se n da pra deletar ou usar pra outra coisa
+
+    // dá para fazer as demais extenderes essa, ai colocamos os métodos que se repetem aqui.
 }

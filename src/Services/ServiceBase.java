caa356144758840a8e80890fcd576ebcdf0985de
Igor Meurer <igorigormeurer@hotmail.com>
2024-08-22 03:50:08 -0300

    Só tirei uma coisa e fiquei tentando fazer o .jar

--- src/Services/ServiceBase.java
+++
@@ -1,9 +0,0 @@
-package Services;
-
-public class ServiceBase {
-
-    //Talvez da pra usar isso como uma base realmente e deixar como uma classe abstrata que tem as funçoes que os outros utilizam
-    // Só pra ficar bonito se n da pra deletar ou usar pra outra coisa
-
-    // dá para fazer as demais extenderes essa, ai colocamos os métodos que se repetem aqui.
-}

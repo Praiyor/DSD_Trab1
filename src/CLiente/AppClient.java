f53d8093ee473f3d376e48a2f5c8640eef2e343d
Igor Meurer <igorigormeurer@hotmail.com>
2024-08-22 12:23:23 -0300

    Resolvido o erro que dava caso o usuario n utilizasse um int no client

--- src/CLiente/AppClient.java
+++ src/CLiente/AppClient.java
@@ -1,6 +1,7 @@
 package CLiente;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import CLiente.Controler.*;
@@ -29,32 +30,37 @@ public class AppClient {
 			System.out.println("3. + opções Tecnico");
 			System.out.println("4. Sair");
 			System.out.print("Opção: ");
+			try {
+				int opcao = s.nextInt();
+				s.nextLine();
 
-			int opcao = s.nextInt();
-			s.nextLine();
-
-			switch (opcao) {
-			case 1:
-				System.out.println("Selecionou 1");
-				opcaoCorredores();
-				continuar = false;
-				break;
-			case 2:
-				System.out.println("Selecionou 3.");
-				opcaoPiloto();
-				continuar = false;
-				break;
-			case 3:
-				System.out.println("Selecionou 4.");
-				opcaoTecnico();
-				continuar = false;
-				break;
-			case 4:
-				continuar = false;
-				break;
-			default:
-				System.out.println("Opção inválida.");
+				switch (opcao) {
+					case 1:
+						System.out.println("Selecionou 1");
+						opcaoCorredores();
+						continuar = false;
+						break;
+					case 2:
+						System.out.println("Selecionou 3.");
+						opcaoPiloto();
+						continuar = false;
+						break;
+					case 3:
+						System.out.println("Selecionou 4.");
+						opcaoTecnico();
+						continuar = false;
+						break;
+					case 4:
+						continuar = false;
+						break;
+					default:
+						System.out.println("Opção inválida.");
+				}
+			}catch (InputMismatchException e){
+				System.out.println("Entrada inválida. Utilize apenas números");
+				s.nextLine();
 			}
+
 		}
 	}
 
@@ -75,123 +81,129 @@ public class AppClient {
 			System.out.println("10. Voltar");
 			System.out.print("Opção: ");
 
-			int opcao = s.nextInt();
-			s.nextLine();
+			try {
+				int opcao = s.nextInt();
+				s.nextLine();
 
-			String torneio = "";
-			String premio = "";
-			String cpfPiloto = "";
-			String cpfTecnico = "";
+				String torneio = "";
+				String premio = "";
+				String cpfPiloto = "";
+				String cpfTecnico = "";
 
-			switch (opcao) {
-			case 1:
-				System.out.println("1. Criar Corredores ");
-				System.out.println("Digite o nome do Torneio");
-				torneio = s.nextLine();
-				System.out.println("Digite o Premio");
-				premio = s.nextLine();
-				System.out.println("Digite o CPF do Pilto");
-				cpfPiloto = s.nextLine();
-				System.out.println("Digite o CPF do Tecnico");
-				cpfTecnico = s.nextLine();
-				System.out.println(torneio + " " + premio);
-				con.enviarMsgCorredores("INSERT", torneio, premio, cpfPiloto, cpfTecnico);
-				continuar = false;
-				opcaoCorredores();
-				break;
-			case 2:
-				System.out.println("2. Atualizar Corredores");
+				switch (opcao) {
+					case 1:
+						System.out.println("1. Criar Corredores ");
+						System.out.println("Digite o nome do Torneio");
+						torneio = s.nextLine();
+						System.out.println("Digite o Premio");
+						premio = s.nextLine();
+						System.out.println("Digite o CPF do Pilto");
+						cpfPiloto = s.nextLine();
+						System.out.println("Digite o CPF do Tecnico");
+						cpfTecnico = s.nextLine();
+						System.out.println(torneio + " " + premio);
+						con.enviarMsgCorredores("INSERT", torneio, premio, cpfPiloto, cpfTecnico);
+						continuar = false;
+						opcaoCorredores();
+						break;
+					case 2:
+						System.out.println("2. Atualizar Corredores");
 
-				System.out.println("Digite o Torneio a ser atualizado");
-				torneio = s.nextLine();
+						System.out.println("Digite o Torneio a ser atualizado");
+						torneio = s.nextLine();
 
-				System.out.println("Digite o premio");
-				premio = s.nextLine();
+						System.out.println("Digite o premio");
+						premio = s.nextLine();
 
-				System.out.println(torneio + " " + premio);
-				con.enviarUpdateCorredores("UPDATE", torneio, premio);
-				continuar = false;
-				opcaoCorredores();
-				break;
-			case 3:
-				System.out.println("3. Obter Corredores.");
+						System.out.println(torneio + " " + premio);
+						con.enviarUpdateCorredores("UPDATE", torneio, premio);
+						continuar = false;
+						opcaoCorredores();
+						break;
+					case 3:
+						System.out.println("3. Obter Corredores.");
 
-				System.out.println("Digite o tornio");
-				torneio = s.nextLine();
+						System.out.println("Digite o tornio");
+						torneio = s.nextLine();
 
-				con.enviarMsg("CORREDORES", "GET", torneio);
-				continuar = false;
-				opcaoCorredores();
-				break;
-			case 4:
-				System.out.println("4. Deletar Torneio");
-				System.out.println("Digite o nome do torneio");
-				torneio = s.nextLine();
-				con.enviarMsg("CORREDORES", "DELETE", torneio);
-				continuar = false;
-				opcaoCorredores();
-				break;
-			case 5:
-				System.out.println("5. Obter todos os Corredores");
+						con.enviarMsg("CORREDORES", "GET", torneio);
+						continuar = false;
+						opcaoCorredores();
+						break;
+					case 4:
+						System.out.println("4. Deletar Torneio");
+						System.out.println("Digite o nome do torneio");
+						torneio = s.nextLine();
+						con.enviarMsg("CORREDORES", "DELETE", torneio);
+						continuar = false;
+						opcaoCorredores();
+						break;
+					case 5:
+						System.out.println("5. Obter todos os Corredores");
 
-				con.getList("CORREDORES");
-				continuar = false;
-				opcaoCorredores();
-				break;
-			case 6:
-				System.out.println("6. Adicionar um piloto aos Corredores");
-				System.out.println("Digite o nome do torneio");
-				torneio = s.nextLine();
+						con.getList("CORREDORES");
+						continuar = false;
+						opcaoCorredores();
+						break;
+					case 6:
+						System.out.println("6. Adicionar um piloto aos Corredores");
+						System.out.println("Digite o nome do torneio");
+						torneio = s.nextLine();
 
-				System.out.println("Digite o cpf do Piloto");
-				cpfPiloto = s.nextLine();
+						System.out.println("Digite o cpf do Piloto");
+						cpfPiloto = s.nextLine();
 
-				con.addOnCorredores("ADDPIL", torneio, cpfPiloto);
+						con.addOnCorredores("ADDPIL", torneio, cpfPiloto);
 
-				continuar = false;
-				opcaoCorredores();
-			case 7:
-				System.out.println("7. Adicionar um tecnico aos Corredores");
-				System.out.println("Digite o nome do torneio");
-				torneio = s.nextLine();
+						continuar = false;
+						opcaoCorredores();
+					case 7:
+						System.out.println("7. Adicionar um tecnico aos Corredores");
+						System.out.println("Digite o nome do torneio");
+						torneio = s.nextLine();
 
-				System.out.println("Digite o CPF do Tecnico");
-				cpfTecnico = s.nextLine();
+						System.out.println("Digite o CPF do Tecnico");
+						cpfTecnico = s.nextLine();
 
-				con.addOnCorredores("ADDTEC", torneio, cpfTecnico);
+						con.addOnCorredores("ADDTEC", torneio, cpfTecnico);
 
-				continuar = false;
-				opcaoCorredores();
-			case 8:
-				System.out.println("8. Remover um piloto aos Corredores");
-				System.out.println("Digite o nome do torneio");
-				torneio = s.nextLine();
-				System.out.println("Digite o cpf do Piloto");
-				cpfPiloto = s.nextLine();
+						continuar = false;
+						opcaoCorredores();
+					case 8:
+						System.out.println("8. Remover um piloto aos Corredores");
+						System.out.println("Digite o nome do torneio");
+						torneio = s.nextLine();
+						System.out.println("Digite o cpf do Piloto");
+						cpfPiloto = s.nextLine();
 
-				con.removeOnCorredores("REMOVEPIL", torneio, cpfPiloto);
+						con.removeOnCorredores("REMOVEPIL", torneio, cpfPiloto);
 
-				continuar = false;
-				opcaoCorredores();
-			case 9:
-				System.out.println("9. Remover um tecnico aos Corredores");
-				System.out.println("Digite o nome do torneio");
-				torneio = s.nextLine();
+						continuar = false;
+						opcaoCorredores();
+					case 9:
+						System.out.println("9. Remover um tecnico aos Corredores");
+						System.out.println("Digite o nome do torneio");
+						torneio = s.nextLine();
 
-				System.out.println("Digite o CPF do Tecnico");
-				cpfTecnico = s.nextLine();
+						System.out.println("Digite o CPF do Tecnico");
+						cpfTecnico = s.nextLine();
 
-				con.removeOnCorredores("REMOVETEC", torneio, cpfTecnico);
+						con.removeOnCorredores("REMOVETEC", torneio, cpfTecnico);
 
-				continuar = false;
-				opcaoCorredores();
-			case 10:
-				continuar = false;
-				opcoes();
-				break;
-			default:
-				System.out.println("Opção inválida.");
+						continuar = false;
+						opcaoCorredores();
+					case 10:
+						continuar = false;
+						opcoes();
+						break;
+					default:
+						System.out.println("Opção inválida.");
+				}
+			}catch (InputMismatchException e){
+				System.out.println("Entrada inválida. Utilize apenas números");
+				s.nextLine();
 			}
+
 		}
 	}
 
@@ -209,82 +221,88 @@ public class AppClient {
 			System.out.println("6. Voltar");
 			System.out.print("Opção: ");
 
-			int opcao = s.nextInt();
-			s.nextLine();
+			try {
+				int opcao = s.nextInt();
+				s.nextLine();
 
-			String cpf;
-			String nome;
-			String endereco;
-			String carro;
+				String cpf;
+				String nome;
+				String endereco;
+				String carro;
 
-			switch (opcao) {
-			case 1:
-				System.out.println("1. Criar Piloto ");
-				System.out.println("Digite o CPF da Piloto");
-				cpf = s.nextLine();
-				System.out.println("Digite o Nome do Piloto");
-				nome = s.nextLine();
-				System.out.println("Digite o Endere�o do Piloto");
-				endereco = s.nextLine();
-				System.out.println("Digite o Carro do Piloto");
-				carro = s.nextLine();
-				System.out.println(nome + " " + cpf + " " + endereco + " " + carro);
-				con.enviarMsgPiloto("INSERT", cpf, nome, endereco, carro);
-				continuar = false;
-				opcaoPiloto();
-				break;
-			case 2:
-				System.out.println("2. Atualizar Piloto");
+				switch (opcao) {
+					case 1:
+						System.out.println("1. Criar Piloto ");
+						System.out.println("Digite o CPF da Piloto");
+						cpf = s.nextLine();
+						System.out.println("Digite o Nome do Piloto");
+						nome = s.nextLine();
+						System.out.println("Digite o Endere�o do Piloto");
+						endereco = s.nextLine();
+						System.out.println("Digite o Carro do Piloto");
+						carro = s.nextLine();
+						System.out.println(nome + " " + cpf + " " + endereco + " " + carro);
+						con.enviarMsgPiloto("INSERT", cpf, nome, endereco, carro);
+						continuar = false;
+						opcaoPiloto();
+						break;
+					case 2:
+						System.out.println("2. Atualizar Piloto");
 
-				System.out.println("Digite o CPF da Piloto a ser atualizado");
-				cpf = s.nextLine();
+						System.out.println("Digite o CPF da Piloto a ser atualizado");
+						cpf = s.nextLine();
 
-				System.out.println("Digite o Nome da Piloto");
-				nome = s.nextLine();
+						System.out.println("Digite o Nome da Piloto");
+						nome = s.nextLine();
 
-				System.out.println("Digite o endereço da Piloto");
-				endereco = s.nextLine();
+						System.out.println("Digite o endereço da Piloto");
+						endereco = s.nextLine();
 
-				System.out.println("Digite o Carro do Piloto");
-				carro = s.nextLine();
+						System.out.println("Digite o Carro do Piloto");
+						carro = s.nextLine();
 
-				System.out.println(nome + " " + cpf + " " + endereco + " " + carro);
-				con.enviarMsgPiloto("UPDATE", cpf, nome, endereco, carro);
-				continuar = false;
-				opcaoPiloto();
-				break;
-			case 3:
-				System.out.println("3. Obter Piloto.");
+						System.out.println(nome + " " + cpf + " " + endereco + " " + carro);
+						con.enviarMsgPiloto("UPDATE", cpf, nome, endereco, carro);
+						continuar = false;
+						opcaoPiloto();
+						break;
+					case 3:
+						System.out.println("3. Obter Piloto.");
 
-				System.out.println("Digite o CPF do Piloto");
-				cpf = s.nextLine();
+						System.out.println("Digite o CPF do Piloto");
+						cpf = s.nextLine();
 
-				con.enviarMsg("PILOTO", "GET", cpf);
-				continuar = false;
-				opcaoPiloto();
-				break;
-			case 4:
-				System.out.println("4. Deletar Piloto");
-				System.out.println("Digite o CPF do Piloto a ser deletado");
-				cpf = s.nextLine();
-				con.enviarMsg("PILOTO", "DELETE", cpf);
-				continuar = false;
-				opcaoPiloto();
-				break;
-			case 5:
-				System.out.println("5. Obter todos os Pilotos");
+						con.enviarMsg("PILOTO", "GET", cpf);
+						continuar = false;
+						opcaoPiloto();
+						break;
+					case 4:
+						System.out.println("4. Deletar Piloto");
+						System.out.println("Digite o CPF do Piloto a ser deletado");
+						cpf = s.nextLine();
+						con.enviarMsg("PILOTO", "DELETE", cpf);
+						continuar = false;
+						opcaoPiloto();
+						break;
+					case 5:
+						System.out.println("5. Obter todos os Pilotos");
 
-				con.getList("PILOTO");
-				continuar = false;
-				opcaoPiloto();
-				break;
-			case 6:
-				continuar = false;
-				opcoes();
-				break;
-			default:
-				System.out.println("Opção inválida.");
+						con.getList("PILOTO");
+						continuar = false;
+						opcaoPiloto();
+						break;
+					case 6:
+						continuar = false;
+						opcoes();
+						break;
+					default:
+						System.out.println("Opção inválida.");
+				}
+			}catch (InputMismatchException e){
+				System.out.println("Entrada inválida. Utilize apenas números");
+				s.nextLine();
 			}
+
 		}
 	}
 
@@ -301,88 +319,117 @@ public class AppClient {
 			System.out.println("6. Voltar");
 			System.out.print("Opção: ");
 
-			int opcao = s.nextInt();
-			s.nextLine();
+			try {
+				int opcao = s.nextInt();
+				s.nextLine();
 
-			String cpf;
-			String nome;
-			String endereco;
-			String especialidade;
-			int experiencia;
+				String cpf;
+				String nome;
+				String endereco;
+				String especialidade;
+				int experiencia;
 
-			switch (opcao) {
-			case 1:
-				System.out.println("1. Criar Tecnico ");
-				System.out.println("Digite o CPF do Tecnico");
-				cpf = s.nextLine();
-				System.out.println("Digite o Nome do Tecnico");
-				nome = s.nextLine();
-				System.out.println("Digite o Endere�o do Tecnico");
-				endereco = s.nextLine();
-				System.out.println("Digite a especialidade do Tecnico");
-				especialidade = s.nextLine();
-				System.out.println("Digite a experiencia do Tecnico");
-				experiencia = s.nextInt();
-				System.out.println(nome + " " + cpf + " " + endereco + " " + experiencia);
-				con.enviarMsgTecnico("INSERT", cpf, nome, endereco, especialidade, experiencia);
-				continuar = false;
-				opcaoTecnico();
-				break;
-			case 2:
-				System.out.println("2. Atualizar Tecnico");
+				switch (opcao) {
+					case 1:
 
-				System.out.println("Digite o CPF do Tecnico a ser atualizado");
-				cpf = s.nextLine();
+						try {
+							System.out.println("1. Criar Tecnico ");
+							System.out.println("Digite o CPF do Tecnico");
+							cpf = s.nextLine();
+							System.out.println("Digite o Nome do Tecnico");
+							nome = s.nextLine();
+							System.out.println("Digite o Endere�o do Tecnico");
+							endereco = s.nextLine();
+							System.out.println("Digite a especialidade do Tecnico");
+							especialidade = s.nextLine();
+							System.out.println("Digite a experiencia do Tecnico");
+							experiencia = s.nextInt();
+							System.out.println(nome + " " + cpf + " " + endereco + " " + experiencia);
+							con.enviarMsgTecnico("INSERT", cpf, nome, endereco, especialidade, experiencia);
+							continuar = false;
+							opcaoTecnico();
+							break;
+						}catch (InputMismatchException e){
+							System.out.println("Entrada inválida. Utilize apenas números");
+							s.nextLine();
 
-				System.out.println("Digite o Nome do Tecnico");
-				nome = s.nextLine();
+							continuar = false;
+							opcaoTecnico();
+							break;
+						}
 
-				System.out.println("Digite o endere�o do Tecnico");
-				endereco = s.nextLine();
 
-				System.out.println("Digite a Expecialidade do Tecnico");
-				especialidade = s.nextLine();
+					case 2:
 
-				System.out.println("Digite a Experiencia do Tecnico");
-				experiencia = s.nextInt();
+						try {
+							System.out.println("2. Atualizar Tecnico");
 
-				System.out.println(nome + " " + cpf + " " + endereco + " " + experiencia);
-				con.enviarMsgTecnico("UPDATE", cpf, nome, endereco, especialidade, experiencia);
-				continuar = false;
-				opcaoTecnico();
-				break;
-			case 3:
-				System.out.println("3. Obter Tecnico.");
+							System.out.println("Digite o CPF do Tecnico a ser atualizado");
+							cpf = s.nextLine();
 
-				System.out.println("Digite o CPF do Tecnico");
-				cpf = s.nextLine();
+							System.out.println("Digite o Nome do Tecnico");
+							nome = s.nextLine();
 
-				con.enviarMsg("TECNICO", "GET", cpf);
-				continuar = false;
-				opcaoTecnico();
-				break;
-			case 4:
-				System.out.println("4. Deletar Tecnico");
-				System.out.println("Digite o CPF do Tecnico a ser deletado");
-				cpf = s.nextLine();
-				con.enviarMsg("TECNICO", "DELETE", cpf);
-				continuar = false;
-				opcaoTecnico();
-				break;
-			case 5:
-				System.out.println("5. Obter todos os Tecnicos");
+							System.out.println("Digite o endere�o do Tecnico");
+							endereco = s.nextLine();
 
-				con.getList("TECNICO");
-				continuar = false;
-				opcaoTecnico();
-				break;
-			case 6:
-				continuar = false;
-				opcoes();
-				break;
-			default:
-				System.out.println("Opção inválida.");
+							System.out.println("Digite a Expecialidade do Tecnico");
+							especialidade = s.nextLine();
+
+							System.out.println("Digite a Experiencia do Tecnico");
+							experiencia = s.nextInt();
+
+							System.out.println(nome + " " + cpf + " " + endereco + " " + experiencia);
+							con.enviarMsgTecnico("UPDATE", cpf, nome, endereco, especialidade, experiencia);
+							continuar = false;
+							opcaoTecnico();
+							break;
+
+						}catch (InputMismatchException e){
+							System.out.println("Entrada inválida. Utilize apenas números");
+							s.nextLine();
+							continuar = false;
+							opcaoTecnico();
+							break;
+						}
+
+					case 3:
+						System.out.println("3. Obter Tecnico.");
+
+						System.out.println("Digite o CPF do Tecnico");
+						cpf = s.nextLine();
+
+						con.enviarMsg("TECNICO", "GET", cpf);
+						continuar = false;
+						opcaoTecnico();
+						break;
+					case 4:
+						System.out.println("4. Deletar Tecnico");
+						System.out.println("Digite o CPF do Tecnico a ser deletado");
+						cpf = s.nextLine();
+						con.enviarMsg("TECNICO", "DELETE", cpf);
+						continuar = false;
+						opcaoTecnico();
+						break;
+					case 5:
+						System.out.println("5. Obter todos os Tecnicos");
+
+						con.getList("TECNICO");
+						continuar = false;
+						opcaoTecnico();
+						break;
+					case 6:
+						continuar = false;
+						opcoes();
+						break;
+					default:
+						System.out.println("Opção inválida.");
+				}
+			} catch (InputMismatchException e){
+				System.out.println("Entrada inválida. Utilize apenas números");
+				s.nextLine();
 			}
+
 		}
 	}
 }
